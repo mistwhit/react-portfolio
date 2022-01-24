9879a5b731520a97ab086cf5252e019f65768a95
Misty Whitenton <mistywhitenton@gmail.com>
2022-01-23 22:57:59 -0600

    Uploaded favicon and added About Me text

--- src/App.js
+++ src/App.js
@@ -3,22 +3,34 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+
+    <header id="header">
+        <h1>Misty Whitenton</h1>
+    </header>
+    <main>
+        <section id="aboutMe">
+            <h2>About Me</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Dictum at tempor commodo ullamcorper a. Convallis posuere morbi leo urna molestie at elementum eu. 
+                Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. 
+                Dignissim convallis aenean et tortor at risus viverra adipiscing at. 
+                Feugiat pretium nibh ipsum consequat. Enim sed faucibus turpis in eu. Aliquam faucibus purus in massa tempor nec. 
+                Mi bibendum neque egestas congue quisque egestas. Congue nisi vitae suscipit tellus. Et malesuada fames ac turpis. 
+                Quisque non tellus orci ac auctor augue mauris. Netus et malesuada fames ac turpis egestas integer eget aliquet.
+            </p>
+            <p>
+                Mi in nulla posuere sollicitudin aliquam ultrices. Consectetur adipiscing elit pellentesque habitant. 
+                Nunc eget lorem dolor sed viverra ipsum. Mauris ultrices eros in cursus turpis. 
+                Gravida in fermentum et sollicitudin ac. Velit aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. 
+                Eget mauris pharetra et ultrices neque ornare aenean euismod elementum. Arcu odio ut sem nulla pharetra diam. 
+                Lorem mollis aliquam ut porttitor. Dui accumsan sit amet nulla facilisi morbi tempus. Mi tempus imperdiet nulla malesuada. 
+                Eleifend quam adipiscing vitae proin sagittis nisl rhoncus. In fermentum posuere urna nec tincidunt praesent semper feugiat.
+            </p>
+        </section>
+    </main>
+</body>
   );
 }
 
