b78b7823505c4b581c9f55ca27ec2af248fb9b7b
Misty Whitenton <mistywhitenton@gmail.com>
2022-01-25 14:44:42 -0600

    Updated header & navigation set up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,50 +1,11 @@
 import React from 'react';
+import Navigation from './Navigation'
 
-function Header({ currentPage, handlePageChange }) {
-  
+const Header = ({ currentPage, handlePageChange }) => {
   return (
-    <ul className="navbar">
-
-    <li className="nav-item">
-      <a
-        href="#about"
-        onClick={() => handlePageChange("AboutMe")}
-        className={currentPage === "About" ? "nav-link active" : "nav-link"}
-      >
-        About
-      </a>
-    </li>
-
-    <li className="nav-item">
-      <a
-        href="#portfolio"
-        onClick={() => handlePageChange("Portfolio")}
-        className={currentPage === "Portfolio" ? "nav-link active" : "nav-link"}
-      >
-        Portfolio
-      </a>
-    </li>
-
-    <li className="nav-item">
-      <a
-        href="#resume"
-        onClick={() => handlePageChange("Resume")}
-        className={currentPage === "Resume" ? "nav-link active" : "nav-link"}
-      >
-        Resume
-      </a>
-    </li>
-
-    <li className="nav-item">
-      <a
-        href="#contact"
-        onClick={() => handlePageChange("Contact")}
-        className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
-      >
-        Contact
-      </a>
-    </li>
-  </ul>
+    <div>
+        <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
+    </div>
   );
 }
 
