6b915ccbfe478473f42127f5cda63421dddf6990
Misty Whitenton <mistywhitenton@aslan.local>
2022-06-09 18:14:24 -0500

    Updated comments

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,13 @@ const Footer = () => {
         <footer class="bg-link text-center ">
             {/* <!-- Grid container --> */}
             <div class="container p-4">
-                {/* <!-- Section: Social media --> */}
+                {/* <!-- Section: Contact Me --> */}
                 <section class="mb-4">
                     {/* <!-- Email --> */}
                     <a class="btn btn-primary btn-floating m-1" href="mailto: mistywhitenton@gmail.com" role="button"><i class="fas fa-envelope me-3"></i></a>
-                    {/* <!-- Github --> */}
+                    {/* <!-- GitHub --> */}
                     <a class="btn btn-primary btn-floating m-1" href="https://github.com/mistwhit" role="button"><i class="fab fa-github"></i></a>
-                    {/* <!-- Linkedin --> */}
+                    {/* <!-- LinkedIn --> */}
                     <a class="btn btn-primary btn-floating m-1" href="https://www.linkedin.com/in/misty-whitenton/" role="button"><i class="fab fa-linkedin-in"></i></a>
                 </section>
             </div>
