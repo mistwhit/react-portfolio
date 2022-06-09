2a86b39174b28ff4f5a95e30dfb2cda38a64ccfb
Misty Whitenton <mistywhitenton@aslan.local>
2022-06-09 18:06:22 -0500

    Footer updates & testing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,10 +9,10 @@ const Footer = () => {
                 <section class="mb-4">
                     {/* <!-- Email --> */}
                     <a class="btn btn-primary btn-floating m-1" href="mailto: mistywhitenton@gmail.com" role="button"><i class="fas fa-envelope me-3"></i></a>
-                    {/* <!-- Linkedin --> */}
-                    <a class="btn btn-primary btn-floating m-1" href="https://www.linkedin.com/in/misty-whitenton/" role="button"><i class="fab fa-linkedin-in"></i></a>
                     {/* <!-- Github --> */}
                     <a class="btn btn-primary btn-floating m-1" href="https://github.com/mistwhit" role="button"><i class="fab fa-github"></i></a>
+                    {/* <!-- Linkedin --> */}
+                    <a class="btn btn-primary btn-floating m-1" href="https://www.linkedin.com/in/misty-whitenton/" role="button"><i class="fab fa-linkedin-in"></i></a>
                 </section>
             </div>
         </footer>
