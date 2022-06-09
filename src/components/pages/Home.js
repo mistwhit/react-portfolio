8776dfae090aa96cd9cd76e4452e9324e3f1a94d
Misty Whitenton <mistywhitenton@Mistys-MacBook-Air.local>
2022-06-09 16:15:32 -0500

    Updated text on homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
         <div>
             <div className="home container text-center justify-content-center">
                 <h1 className="home-text mb-1">MISTY WHITENTON</h1>
-                <h3 className="mb-5"><em>Full-Stack Developer and Musician</em></h3>
+                <h3 className="mb-5"><em>Full-Stack Web Developer</em></h3>
                 <div className=" text-center">
                     <img src={headshot} alt="Headshot1" className="headshot1 img-fluid" />
                 </div>
