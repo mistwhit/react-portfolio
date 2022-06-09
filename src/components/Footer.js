78c09d8b908509dfcba19856af1c6f57fd3df463
Misty Whitenton <mistywhitenton@Mistys-MacBook-Air.local>
2022-06-09 17:43:34 -0500

    Updated footer order and appearance

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,12 +7,12 @@ const Footer = () => {
             <div class="container p-4">
                 {/* <!-- Section: Social media --> */}
                 <section class="mb-4">
+                    {/* <!-- Email --> */}
+                    <a class="btn btn-primary btn-floating m-1" href="mailto: mistywhitenton@gmail.com" role="button"><i class="fas fa-envelope me-3"></i></a>
                     {/* <!-- Linkedin --> */}
                     <a class="btn btn-primary btn-floating m-1" href="https://www.linkedin.com/in/misty-whitenton/" role="button"><i class="fab fa-linkedin-in"></i></a>
                     {/* <!-- Github --> */}
                     <a class="btn btn-primary btn-floating m-1" href="https://github.com/mistwhit" role="button"><i class="fab fa-github"></i></a>
-                    {/* <!-- Email --> */}
-                    <a class="btn btn-primary btn-floating m-1" href="mailto: mistywhitenton@gmail.com" role="button"><i class="fas fa-envelope me-3"></i></a>
                 </section>
             </div>
         </footer>
