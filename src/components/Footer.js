f3887a45710d31886f36864e51a85cb12aa9e6d7
mistwhit <mistywhitenton@gmail.com>
2022-06-18 17:05:29 -0500

    Links in footer now open in a new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,16 +4,15 @@ const Footer = () => {
     return (
         <footer class="bg-link text-center ">
             {/* <!-- Grid Container --> */}
-            {/* TEST */}
             <div class="container p-4">
                 {/* <!-- Section: Contact Me --> */}
                 <section class="mb-4">
                     {/* <!-- Email --> */}
-                    <a class="btn btn-primary btn-floating m-1" href="mailto: mistywhitenton@gmail.com" role="button"><i class="fas fa-envelope me-3"></i></a>
+                    <a class="btn btn-primary btn-floating m-1" href="mailto: mistywhitenton@gmail.com" target="_blank" rel="noopener noreferrer" role="button"><i class="fas fa-envelope me-3"></i></a>
                     {/* <!-- GitHub --> */}
-                    <a class="btn btn-primary btn-floating m-1" href="https://github.com/mistwhit" role="button"><i class="fab fa-github"></i></a>
+                    <a class="btn btn-primary btn-floating m-1" href="https://github.com/mistwhit" target="_blank" rel="noopener noreferrer" role="button"><i class="fab fa-github"></i></a>
                     {/* <!-- LinkedIn --> */}
-                    <a class="btn btn-primary btn-floating m-1" href="https://www.linkedin.com/in/misty-whitenton/" role="button"><i class="fab fa-linkedin-in"></i></a>
+                    <a class="btn btn-primary btn-floating m-1" href="https://www.linkedin.com/in/misty-whitenton/" target="_blank" rel="noopener noreferrer" role="button"><i class="fab fa-linkedin-in"></i></a>
                 </section>
             </div>
         </footer>
